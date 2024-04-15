089a4509344a17b9882c2e094929fc1609b6624f
Atif Ahmad <40195767+AatifAhmad01@users.noreply.github.com>
2024-04-15 17:03:33 +0500

    add some more endpoints

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -228,7 +228,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
     }
 })
 
-const changePassword = asyncHandler(async (req, res) => {
+const changeCurrentPassword = asyncHandler(async (req, res) => {
     const { oldPassword, newPassword } = req.body;
 
     const user = User.findById(body.user?._id);
@@ -246,8 +246,155 @@ const changePassword = asyncHandler(async (req, res) => {
     res
         .status(200)
         .json(new ApiResponse(200, {}, "Password Changed Succesfully"));
+})
 
+const getCurrentUser = asyncHandler(async (req, res) => {
+    res
+        .status(200)
+        .json(new ApiResponse(200, res.user, "Current user fetched successfully"))
+})
 
+const updateAccountDetails = asyncHandler(async (req, res) => {
+    const { fullName, email } = req.body;
+
+    if (!fullName || !email) {
+        throw new ApiError(400, "All fields are required");
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(body.user?._id,
+        {
+            $set: {
+                fullName,
+                email
+            }
+        },
+        { new: true })
+        .select("-password")
+
+    return res.status(200)
+        .json(new ApiResponse(200, updatedUser, "User updated succefully"))
+})
+
+const updateAvatar = asyncHandler(async (req, res) => {
+
+    const avatarLocalPath = req.file?.path;
+
+    if (!avatarLocalPath) {
+        throw new ApiError(400, "Avatar File Missing");
+    }
+
+    const avatar = await uploadCloudinary(avatarLocalPath);
+
+    if (!avatar.url) {
+        throw new ApiError(400, "Error while uploading file");
+    }
+
+    const user = await User.findOneAndUpdate(body.user?._id,
+        {
+            $set: {
+                avatar: avatar.url
+            }
+        },
+        { new: true }).select("-password");
+
+    return res.status(200)
+        .json(new ApiResponse(200, user, "Avatar Updated."))
+
+})
+
+const updateCoverImage = asyncHandler(async (req, res) => {
+
+    const coverImageLocalPath = req.file?.path;
+
+    if (!coverImageLocalPath) {
+        throw new ApiError(400, "Cover Image File Missing");
+    }
+
+    const coverImage = await uploadCloudinary(coverImageLocalPath);
+
+    if (!coverImage.url) {
+        throw new ApiError(400, "Error while uploading file");
+    }
+
+    const user = await User.findOneAndUpdate(body.user?._id,
+        {
+            $set: {
+                coverImage: coverImage.url
+            }
+        },
+        { new: true }).select("-password");
+
+    return res.status(200)
+        .json(new ApiResponse(200, user, "Cover Image Updated."))
+
+})
+
+const getUserChennalProfile = asyncHandler(async (req, res) => {
+    {
+        const { userName } = req.params;
+
+        if (!userName?.trim()) {
+            throw new ApiError(400, "UserName not found");
+        }
+
+        const chennal = User.aggregate([
+
+            {
+                $match: {
+                    userName: userName?.toLowerCase()
+                }
+            },
+            {
+                $lookup: {
+                    from: "subsciptions",
+                    localField: "_id",
+                    foreignField: "channel",
+                    as: "subscribers"
+                },
+                $lookup: {
+                    from: "subsciptions",
+                    localField: "_id",
+                    foreignField: "subscriber",
+                    as: "subscribedTo"
+                },
+                $addFields: {
+                    subscriberCount: {
+                        $size: "$subscribers"
+                    },
+                    channelsSubscribedToCount: {
+                        $size: "$subscribedTo"
+                    },
+                    isSubscribed: {
+                        $cond: {
+                            if: { $in: [req.user?._id, "$subscribers.subscriber"] },
+                            then: true,
+                            else: false
+                        }
+                    }
+                }
+            },
+            {
+                fullName: 1,
+                userName: 1,
+                subscriberCount: 1,
+                channelsSubscribedToCount: 1,
+                isSubscribed: 1,
+                avatar: 1,
+                coverImage: 1,
+                email: 1,
+            }
+        ])
+
+        console.log(res);
+
+        if (!chennal.length) {
+            throw new ApiError(400, "Chennal does not exists")
+        }
+
+        return res.status(200).json(
+            new ApiResponse(200, chennal[0], "User Channel fetched successfully")
+        )
+    }
 })
 
 export {
@@ -255,5 +402,10 @@ export {
     loginUser,
     logoutUser,
     refreshAccessToken,
-    changePassword
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateAvatar,
+    updateCoverImage,
+    getUserChennalProfile
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword } from "../controllers/user.controller.js";
+import { registerUser, loginUser, logoutUser, refreshAccessToken, changePassword, changeCurrentPassword } from "../controllers/user.controller.js";
 import { upload } from "../middlwares/multer.middlware.js";
 import { verifyUser } from "../middlwares/auth.middlware.js";
 
@@ -22,7 +22,7 @@ router.route("/login").post(loginUser);
 
 //Secure routes //User needs to be logged in
 router.route("/logout").post(verifyUser, logoutUser)
-router.route("/changePassword").post(verifyUser, changePassword);
+router.route("/changePassword").post(verifyUser, changeCurrentPassword);
 
 router.route("/refreshAccessToken").post(refreshAccessToken);
 
